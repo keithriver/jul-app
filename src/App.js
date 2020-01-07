2bf6432c752ff31cbc8bfc8f48eb407e55cc9477
Jul <Jul>
2020-01-07 21:49:19 +0300

    login/logout

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ import "./App.css";
 
 
 
-function App() {
- 
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: true
+    };
+    this.changeState = this.changeState.bind(this);
+  };
+
+  changeState = () => {
+    this.setState({
+      login: !this.state.login
+    });
+  };
+  
+  render() {
+  const { login } = this.state;
+
   return (
     
     <div className = 'header'>
       <div className = 'logo'>jul-app</div>
-      <button className = 'headButton'> 
-        Login 
+      <button 
+      className = 'headButton'
+      onClick={this.changeState}
+      > 
+        {login ? "Login" : "Log Out"} 
       </button>
     </div>
   );
 }
 
+}
 export default App;
