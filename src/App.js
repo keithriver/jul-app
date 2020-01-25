b23ff6a02880928e819ae6d0d33a5398af2a1d6d
Jul <Jul>
2020-01-26 00:42:27 +0300

    added body with great colors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: true
+      isLoggedIn: false
     };
     this.changeState = this.changeState.bind(this);
   }
@@ -20,12 +20,27 @@ class App extends React.Component {
     const { isLoggedIn } = this.state;
 
     return (
-      <div className="header">
-        <div className="logo">jul-app</div>
-        <button className="headButton" onClick={this.changeState}>
-          {isLoggedIn ? "Login" : "Log Out"}
-        </button>
-        <div>{isLoggedIn ? <div>Welcome!</div> : null}</div>
+      <div className="page">
+        <div className="header">
+          <div className="logo">jul-app</div>
+          <div>{isLoggedIn ? <div>Welcome!</div> : null}</div>
+          <button className="headButton" onClick={this.changeState}>
+            {isLoggedIn ? "Logout" : "Login"}
+          </button>
+        </div>
+        <div className="body">
+          <div className="form">
+            <div className="input">
+              <input />
+              <div>username</div>
+            </div>
+            <div className="input">
+              <input />
+              <div>password</div>
+            </div>
+          </div>
+        </div>
+        <div className="footer">Footer</div>
       </div>
     );
   }
