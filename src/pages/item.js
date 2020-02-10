1c72af448106e1068752809f5d2b0f18bfa76b45
Jul <Jul>
2020-02-10 23:36:42 +0300

    styled item page (a bit)

--- src/pages/item.js
+++ src/pages/item.js
@@ -1,21 +1,30 @@
 import React from "react";
+import "./item.css";
 const grammsKey = "nutrition-per-100g";
 const mlKey = "nutrition-per-100ml";
 
 export const Item = ({ item }) => {
   const nutritionParams = item[grammsKey] ? item[grammsKey] : item[mlKey];
-  const labels = { energy: "Энергетическая ценность", protein: "Белок" };
+  console.log(nutritionParams);
+  const labels = {
+    energy: "Energy",
+    protein: "Protein",
+    fat: "Fat",
+    "saturated-fat": "Saturated fat",
+    carbohydrate: "Carbohydrate",
+    sugars: "Sugars",
+    "dietary-fibre": "Dietary fibre",
+    sodium: "Sodium"
+  };
   return (
     <div className="food-item">
-      {item.name}
-      <div className="bullet" />
-      <div className="list-item-header">{item.id}</div>
+      <div className="food-name">{item.name}</div>
       <div>
         {Object.keys(nutritionParams).map(key => {
           return (
-            <div>
-              <div>{labels[key]}</div>
-              <div>{nutritionParams[key]}</div>
+            <div className="food-parameter">
+              <div className="label">{labels[key]}:</div>
+              <div className="value">{nutritionParams[key]}</div>
             </div>
           );
         })}
